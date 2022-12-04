37b7c9643b7dac0410f737cccd504e68fe32a941
Abates08 <102253785+Abates08@users.noreply.github.com>
2022-12-04 22:25:00 +0000

    h

--- 4by5chars.cc
+++ 4by5chars.cc
@@ -6,6 +6,7 @@ int charamount = 0;
 int firsttimedone = 0;
 /*char ex[number of big elements][number of elements in 1st][number of elements in 2nd]*/
 char layers[5][64];
+int number1s[26] = {1, 4, 7, 8, 9, 10, 13, 19, 25, 26, 27, 28, 35, 38, 41, 42, 43, 44, 47, 48, 49, 50, 53, 59, 60, 61};
 char c[10][5][6];
 void init(){
   //small spaces
@@ -42,41 +43,28 @@ void init(){
   c[10][1, 5][5] = ' ';
   c[10][2, 3, 4][2, 3] = ' ';
   layers[1, 2, 3, 4, 5][31, 32, 33, 34] = ' ';
-  layers[1][1,4,8,9,10,13,19,25,26,27,28,35,38,41,42,43,44,47,48,49,50,53,59,60,61] = '1';
-  layers[1][2,3,5,6,7,11,12,14,15,16,17,18,20,21,22,23,24,29,30,31,32,33,34,36,37,39,40,45,46,51,52,54,55,56,57,58,62,63,64] = ' ';
+  layers[1][1] = '1';
+  layers[1][2] = ' ';
 }
-/*void nameline(){
-  /for every new character, increase charamount by one and convert chars to layers.*
-  charamount = 10;
-  for(int i = 0; i < charamount; i++){
-    for(int j = 0; j < 5; j++){
-      for(int k = 0; k < 6; k++){
-        if(firsttimedone == 0){
-          if(layers[i][k] != '\0'){
-            layers[i][k] = c[i][j][k];
-          }
-        }else{
-          if(layers[i][k + x] != '\0'){
-            layers[i][k + x] = c[i][j][k];
-            x++;
-          }
+void nameline(){
+  for(int i = 0; i < 64; i++){
+    if(i == number1s[i]){
+      layers[1][i] = '1';
+   } else{
+      layers[1][i] = ' ';
 
-        }
-        
-        
-      }
-      firsttimedone = 1;
     }
   }
-}*/
+}
 int main(){
   printf("hello world");
    init();
    //nameline();
    /*name the char array */
      for(int i = 0; i < 5; i++){
-       for(int j = 0; j < 6; j++){
-         printf("%c", layers[i][j]);
+       for(int j = 0; j < 64; j++){
+        char y = layers[i][j];
+         printf("%c", y);
        }
      printf("\n");
     }
